ef9a84529538117134cedc7b918ff2e7dac41409
Salvatore Allegra <salallegra@gmail.com>
2026-05-26 19:55:27 -0700

    Add useState hooks and event handlers to rating component

--- src/components/ratings.jsx
+++ src/components/ratings.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
+
 const Ratings = () => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
   return (
     <div className="rating-container">
-      <h2>Ratings ye</h2>
+      <h2>Rate your experience</h2>
+      <div className="stars">
+        {[1, 2, 3, 4, 5].map((star) => (
+          <span
+            onClick={() => setRating(star)}
+            onMouseEnter={() => setHover(star)}
+            onMouseLeave={() => setHover(0)}
+            key={star}
+            className="star"
+          >
+            &#9733;
+          </span>
+        ))}
+      </div>
     </div>
   );
 };
